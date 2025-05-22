0af623abf9e8b4e141cad279e9bf3a3919c0310f
rmst <rmst@qq.com>
2025-05-22 21:58:09 +0800

    test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,9 +29,7 @@ async fn check_update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()>
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_updater::Builder::new().build())
-    .plugin(tauri_plugin_opener::init())
     .invoke_handler(tauri::generate_handler![check_update])
     .setup(|app| {
       #[cfg(desktop)]
