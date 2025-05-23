5074914f5a944d3b909b8a6b8c34337c207959bc
rmstsd <rmstsd@qq.com>
2025-05-23 15:51:45 +0800

    tauri-plugin-process

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -43,6 +43,7 @@ fn get_version(app: AppHandle) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_updater::Builder::new().build())
     .invoke_handler(tauri::generate_handler![check_update, get_version])
     .setup(|app| {
